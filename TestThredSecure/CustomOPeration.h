cc1b18fc6f7804415be651e049f54c492a3bf531
JCCP3 <616613995@qq.com>
2015-09-11 13:52:11 +0800

    提交

---
+++ TestThredSecure/CustomOPeration.h
@@ -0,0 +1,13 @@
+//
+//  CustomOPeration.h
+//  TestThredSecure
+//
+//  Created by JC_CP3 on 15/9/11.
+//  Copyright (c) 2015年 JC_CP3. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CustomOPeration : NSOperation
+
+@end
---
+++ TestThredSecure/CustomOPeration.m
@@ -0,0 +1,18 @@
+//
+//  CustomOPeration.m
+//  TestThredSecure
+//
+//  Created by JC_CP3 on 15/9/11.
+//  Copyright (c) 2015年 JC_CP3. All rights reserved.
+//
+
+#import "CustomOPeration.h"
+
+@implementation CustomOPeration
+
+- (void)main
+{
+    
+}
+
+@end
